Fixed normalization problem in exercise 2. Almost finished.
Co-authored-by: LucianoNeimark <lucianoneimark@gmail.com>

TP3/src/ejercicio2.py
@@ -2,57 +2,40 @@ import numpy as np
 
 from utils import *
 import pandas as pd
-
+from normalization import *
 
 def main():
-    TRAINING_SIZE = 15
+    df = pd.read_csv('/Users/pazaramburu/Desktop/SIA-TP-S/TP3/data/TP3-ej2-conjunto.csv')
 
-    df = pd.read_csv('/Users/motegui/Documents/GitHub/SIA-TP-S/TP3/data/TP3-ej2-conjunto.csv')
-
-    x_values_train = df[['x1', 'x2', 'x3']].values
-    y_values_train = df['y'].values
+    x_values = df[['x1', 'x2', 'x3']].values
+    y_values = df['y'].values
 
     # Normalizar los datos entre -1 y 1 utilizando la interpolación de NumPy
-    x_values_train_normalized = np.interp(x_values_train, (x_values_train.min(), x_values_train.max()), (0, 1)).tolist()
-    y_values_train_normalized = np.interp(y_values_train, (y_values_train.min(), y_values_train.max()), (0, 1)).tolist()
+    x_min = x_values.min()
+    x_max = x_values.max()
+    y_min = y_values.min()
+    y_max = y_values.max()
+    # x_values_normalized = np.interp(x_values, (x_min, x_max), (-1, 1)).tolist()
+    x_values = x_values.tolist()
+    y_values_normalized = normalize(y_values, 0, 1)
 
-    # x_values_train = np.array(x_values[:])
-    # y_values_train = np.array(y_values[:])
+    split_index = int(len(x_values) * 0.8)  # 80% de los datos para train
+    x_train, x_test = x_values[:split_index], x_values[split_index:]
+    y_train, y_test = y_values_normalized[:split_index], y_values_normalized[split_index:]
 
-    # [w, err] = lineal_nonlineal_perceptron(x_values_train, y_values_train, random_initialize_weight, compute_error, 10000,
-    #                                        0.02,
-    #                                        0, lineal_theta, lineal_prime_theta)
+    #entrenamos el perceptron
+    [w, err_train] = lineal_nonlineal_perceptron(x_train, y_train, random_initialize_weight, compute_error_nonlinear,
+                                                 10000, 0.01, 0, logistic_theta, logistic_prime_theta,
+                                                 [y_min, y_max])
 
-    # El error dio muy alto por lo que no es lineal. A medida que agrandamos el training size da mas alto el error
-    # normalized_data = np.interp(data, (min_val, max_val), (new_min, new_max))
-    # normalized_data = np.interp(x_values_train, (min(x_values_train), max(x_values_train)), (-1, 1))
+    # rendimiento en conjunto train -> capacidad de aprendizaje
+    print('Error en el conjunto de entrenamiento:', err_train)
 
-    randomNum1 = random.randint(0, len(x_values_train_normalized)-1)
-    randomNum2 = random.randint(0, len(x_values_train_normalized)-1)
-    randomNum3 = random.randint(0, len(x_values_train_normalized)-1)
-
-    test_X = [x_values_train_normalized[randomNum1], x_values_train_normalized[randomNum2], x_values_train_normalized[randomNum3]]
-    test_y = [y_values_train_normalized[randomNum1], y_values_train_normalized[randomNum2], y_values_train_normalized[randomNum3]]
-
-    [w, err] = lineal_nonlineal_perceptron(test_X,
-                                           test_y,
-                                           random_initialize_weight, compute_error,
-                                           100,
-                                           0.1,
-                                           0, logistic_theta, logistic_prime_theta)
-
-    print('error is: ', err)
-
-    # x_values_train = x_values[TRAINING_SIZE:]
-    # y_values_train = y_values[TRAINING_SIZE:]
-
-
-
-    for x, y in zip(x_values_train_normalized,
-                    y_values_train_normalized):
-        print(sum(np.multiply(w, [1] + x) - y))
+    # rendimiento en el conjunto de prueba -> capacidad de generalizacion
+    y_test = denormalize(y_test, y_min, y_max)
+    err_test = test_error([x_test, y_test], w, logistic_theta, [y_min, y_max])
+    print('Error en el conjunto de prueba:', err_test)
 
 
 if __name__ == '__main__':
     main()
-

TP3/src/utils.py
@@ -2,6 +2,7 @@ import math
 import random
 
 import numpy as np
+from normalization import *
 
 
 def staggered_perceptron(input_data, expected_output, initialize_weight, compute_error_function, limit, step):
@@ -25,7 +26,7 @@ def staggered_perceptron(input_data, expected_output, initialize_weight, compute
 
 
 def lineal_nonlineal_perceptron(input_data, expected_output, initialize_weight, compute_error_function, limit, step,
-                                epsilon, theta, theta_prime):
+                                epsilon, theta, theta_prime, min_max):
     i = 0
     w = initialize_weight(len(input_data[0]))
     min_error = math.inf
@@ -38,7 +39,7 @@ def lineal_nonlineal_perceptron(input_data, expected_output, initialize_weight, 
         # delta_w = new_formula
         delta_w = np.multiply(step * (expected_output[u] - o) * theta_prime(h), x)
         w += delta_w
-        error = compute_error_function([input_data, expected_output], w, theta)
+        error = compute_error_function([input_data, expected_output], w, theta, min_max)
         if error < min_error:
             min_error = error
             w_min = w.copy()
@@ -50,9 +51,27 @@ def random_initialize_weight(dim):
     return [random.random() for _ in range(dim + 1)]
 
 
-def compute_error(data, w, theta):
+def compute_error(data, w, theta, min_max):
     t = np.sum(np.square(np.subtract(data[1], real_output(data[0], w, theta))))
     return 0.5 * t
+
+
+def compute_error_nonlinear(data, w, theta, min_max):
+    input_data, expected_output = data
+    # unormalized_input = np.interp(input_data, (0, 1), (min_max[0], min_max[1])).tolist()
+    # unormalized_output = denormalize(expected_output, min_max[2], min_max[3])
+    predicted_output = real_output(input_data, w, theta)
+    # unormalized_predicted = denormalize(predicted_output, min_max[2], min_max[3])
+    error = np.sum(np.square(np.subtract(expected_output, predicted_output)))
+    return error * 0.5
+
+
+def test_error(data, w, theta, min_max):
+    input_data, expected_output = data
+    predicted_output = real_output(input_data, w, theta)
+    unormalized_predicted = denormalize(predicted_output, min_max[0], min_max[1])
+    error = np.sum(np.square(np.subtract(expected_output, unormalized_predicted)))
+    return error * 0.5
 
 
 def real_output(input_data, w, theta):
@@ -80,7 +99,7 @@ def hyp_tan_theta(value):
 
 
 def hyp_tan_prime_theta(value):
-    return BETA * (1 - np.square(np.tanh(BETA*value)))
+    return BETA * (1 - np.square(hyp_tan_theta(value)))
 
 
 def logistic_theta(value):