2d16ff699518038d2401bca2c65b091bf7552570
ntordomar <95659618+ntordomar@users.noreply.github.com>
2024-05-01 15:14:27 -0300

    refactor code. Finished ej2 Co-Author: paramburu22 <paramburu@itba.edu.ar>

--- TP3/src/ejercicio2.py
+++ TP3/src/ejercicio2.py
@@ -1,29 +1,19 @@
-import numpy as np
-
-from utils import *
+from TP3.src.kfold import kfold_cross_validation
+from TP3.src.theta_functions import thetas, prime_thetas
 import pandas as pd
-from normalization import *
 
 from TP3.config import config
 
-thetas = {
-    "logistic": logistic_theta,
-    "tanh": hyp_tan_theta
-}
-
-prime_thetas = {
-    "logistic": logistic_prime_theta,
-    "tanh": hyp_tan_prime_theta
-}
 
 def main():
-    df = pd.read_csv('/Users/lucho/Desktop/Repos/SIA-TP-S/TP3/data/TP3-ej2-conjunto.csv')
-
+    df = pd.read_csv('/Users/nicolastordomar/Desktop/SIA-TP-S/TP3/data/TP3-ej2-conjunto.csv')
+    df = df.sample(frac=1)
     x_values = df[['x1', 'x2', 'x3']].values
     y_values = df['y'].values
-    error_test, error_train = kfold_cross_validation([x_values, y_values], thetas.get(config.get("theta").get("name")),
+    error_train, error_test = kfold_cross_validation([x_values, y_values], thetas.get(config.get("theta").get("name")),
                                                      prime_thetas.get(config.get("theta").get("name")), config.get("K"))
-    print(error_test, "\n", error_train)
+    print(error_train, "\n", error_test)
+
 
 if __name__ == '__main__':
     main()
