8845e8ad90dda9a431f077dc32744f84b6a77c9b
Luciano <lucianoneimark@gmail.com>
2024-05-02 10:27:44 -0300

    Connected layers and added a method that initializes weights on each neuron

--- TP3/src/Layer.py
+++ TP3/src/Layer.py
@@ -1,6 +1,11 @@
+from utils import *
+
+
 class Layer:
-    def __init__(self, neurons):
+    def __init__(self, neurons, prev_layer=None, next_layer=None):
         self.neurons = neurons
+        self.prev_layer = prev_layer
+        self.next_layer = next_layer
 
     def forward(self, inputs):
         out = []
@@ -8,3 +13,8 @@ class Layer:
             neuron.set_inputs(inputs)
             out.append(neuron.compute_excitement())
         return out
+
+    def populate_weights(self):
+        for neuron in self.neurons:
+            weights = random_initialize_weight(len(self.prev_layer.neurons))
+            neuron.set_weights(weights)  # aca hay que agregar el bias?
