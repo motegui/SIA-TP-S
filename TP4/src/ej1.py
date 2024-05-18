95715e077b7d7740e1128c677e893e16a3034c28
Maria Otegui <motegui@itba.edu.ar>
2024-05-18 13:00:23 -0300

    cleanup ej1

--- TP4/src/ej1.py
+++ TP4/src/ej1.py
@@ -47,8 +47,7 @@ def main():
 
     #print(t[1])
     #-------
-    # Crear la matriz de interacciones entre países
-    # coincidence_matrix = np.zeros((len(countries), len(countries)), dtype=int)
+   #diccionario
     coincidence_matrix = {}
 
     # Ejecutar el algoritmo de Kohonen 100 veces
@@ -58,8 +57,6 @@ def main():
         # obtengo los grupos de paises para cada iteracion
         _, matrix = network.test(input_data)
 
-        # Actualizar la matriz de interacciones, lo q hago es itero sobre cada pais en la lista completa de countries
-        #verifico si country_i y country_j estan en el mismo grupo ( si son iguales tambien deberia sumar)
         for row in matrix:
             for associated_countries in row:
                 associated_countries_list = [country.replace(' ', '') for country in associated_countries.split('  ')]
@@ -76,18 +73,17 @@ def main():
             print("Empty table")
             return
 
-        # Determine the maximum width for formatting
+        #determino el maximo ancho para el formato
         max_key_length = max(len(str(key)) for key in headers)
         max_val_length = max(len(str(val)) for row in data.values() for val in row.values())
 
-        # Calculate column width
+        #calculo el ancho de la columna
         col_width = max(max_key_length, max_val_length)
 
-        # Print header row
+        #imprimo el header de la columna
         header_row = f"{'':>{col_width}} " + " ".join(f"{header:>{col_width}}" for header in headers)
         print(header_row)
 
-        # Print each row
         for row_header in headers:
             row = data[row_header]
             row_str = f"{row_header:>{col_width}} " + " ".join(
@@ -100,25 +96,5 @@ def main():
     print_sorted_table(coincidence_matrix)
     pass
 
-
-
-    # print('{: <15}'.format(''), end='')
-    # for country in countries:
-    #     print('{: <15}'.format(country), end='')
-    # print()
-    #
-    # for i, country_i in enumerate(countries):
-    #     print('{: <15}'.format(country_i), end='')
-    #     for j, country_j in enumerate(countries):
-    #         print('{: <15}'.format(matrix[i, j]), end='')
-    #     print()
-
-
-
-
 if __name__ == '__main__':
     main()
-
-# Belgium  Croatia  Denmark  Estonia  Netherlands  Slovakia  Slovenia  Switzerland
-#
-# Finland  Germany  Italy  Norway  Poland  Spain  Sweden  Ukraine
